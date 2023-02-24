e9448fc8f432042057698dc5367eda10dacadb45
Prabhat-IOT-2103150 <126236690+Prabhat-IOT-2103150@users.noreply.github.com>
2023-02-24 13:59:35 +0530

    Add files via upload

---
+++ type program.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  1 17:15:12 2023
+
+@author: aryan
+"""
+
+a,b,c,d,e=18,25.8,'abc',True,90
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
